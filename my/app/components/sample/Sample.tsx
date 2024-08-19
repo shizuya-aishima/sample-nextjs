38da1f5eb975571bd50509828edbcc5a4c936385
shizuya-aishima <shizuya-aishima@kccs.co.jp>
2024-08-19 15:06:54 +0000

    :+1: update: use client 導入

--- my/app/components/sample/Sample.tsx
+++ my/app/components/sample/Sample.tsx
@@ -1,6 +1,7 @@
 'use server';
 
 import { fetchCount } from '@/lib/features/counter/counterAPI';
+import React from 'react';
 type SsrComponentProps = {
   onClick: () => void;
 };
@@ -10,8 +11,11 @@ const SsrComponent = async ({ onClick }: SsrComponentProps) => {
   return (
     <form action={onClick}>
       {data}
+      <input type="url" />
       <input type="text" />
-      <input type="text" />
+      <button className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
+        Button
+      </button>
       <button className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
         Button
       </button>
