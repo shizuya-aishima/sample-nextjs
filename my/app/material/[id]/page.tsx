eb97c6d46d13bbf9dc39270d8dc4b65f3364ad22
shizuya-aishima <shizuya-aishima@kccs.co.jp>
2024-09-08 06:25:10 +0000

    :+1: update: 検索を実装

--- my/app/material/[id]/page.tsx
+++ my/app/material/[id]/page.tsx
@@ -1,10 +1,18 @@
+import { db } from '@/lib/firebase/init';
+import { doc, getDoc } from 'firebase/firestore';
+import { materialConverter } from '../types';
+
 export function generateStaticParams() {
   let slugs = ['test1'];
   return slugs.map((slug) => ({ id: slug }));
 }
 
-const MaterialEdit = ({ params: { id } }: { params: { id: string } }) => {
-  return <div className="card">{id}</div>;
+const MaterialEdit = async ({ params: { id } }: { params: { id: string } }) => {
+  const docRef = (
+    await getDoc(doc(db, 'material', id).withConverter(materialConverter))
+  ).data();
+
+  return <div className="w-full">{docRef?.name}</div>;
 };
 
 export default MaterialEdit;
