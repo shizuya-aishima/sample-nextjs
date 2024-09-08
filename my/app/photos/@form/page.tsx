90bfac42ee0c3949d09f9196e101b4311fc58118
shizuya-aishima <shizuya-aishima@kccs.co.jp>
2024-09-08 05:33:06 +0000

    :+1: update: sample 追加

---
+++ my/app/photos/@form/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+const Page = () => {
+  return <form className="flex w-full flex-col gap-2">form</form>;
+};
+
+export default Page;
---
+++ my/app/photos/layout.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+import { Suspense } from 'react';
+
+const Layout = ({
+  children,
+  form,
+  // table,
+}: {
+  children: React.ReactNode;
+  form: React.ReactNode;
+  // table: React.ReactNode;
+}) => {
+  let photos = Array.from({ length: 6 }, (_, i) => i + 1);
+  return (
+    <section className="cards-container">
+      {photos.map((id) => (
+        <Link className="card" key={id} href={`/photos/${id}`} passHref>
+          {id}
+        </Link>
+      ))}
+      {children}
+      {form}
+      {/* {table} */}
+    </section>
+  );
+};
+export default Layout;
