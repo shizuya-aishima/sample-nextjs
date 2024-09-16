a82efd92204fc02cb12308741ca4a5d6aa1a2336
shizuya-aishima <shizuya-aishima@kccs.co.jp>
2024-09-16 15:34:41 +0000

    :+1: update: onclick のイベント削除

--- my/app/material/edit/[id]/Form.tsx
+++ my/app/material/edit/[id]/Form.tsx
@@ -64,7 +64,7 @@ export const MaterialEditForm = ({ data, onClick }: MaterialEditFormProsp) => {
           type="number"
         />
       </div>
-      <PrimaryButton onClick={() => {}}>登録</PrimaryButton>
+      <PrimaryButton>登録</PrimaryButton>
     </form>
   );
 };
